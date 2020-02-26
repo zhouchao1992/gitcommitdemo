827f446270a9911e0d43aa75f20b276d869bf7fe
zhouchao1992 <1669028439@qq.com>
2020-02-26 15:05:30 +0800

    提交本地代码到git上步骤测试

---
+++ src/main/java/com/example/gitcommittest/GitcommittestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcommittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcommittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcommittestApplication.class, args);
+    }
+
+}
